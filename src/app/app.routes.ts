92217fdc23734c28269ebe72742edf2e0bdf85ab
Martin Hartfelder <contato@nanosync.dev>
2024-05-16 23:54:47 -0300

    feat: conclusão de template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./core/page/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: "Home Template",
+    path: "",
+    component: HomeComponent
+  }
+];
